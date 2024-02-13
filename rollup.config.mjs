ec042da6924f7f264d0adca6a6f0f37101e8fdb5
X3ZvaWQ <x3zvawq@gmail.com>
2024-02-14 04:29:26 +0800

    fix: build & publish

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,29 +1,38 @@
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import copy from 'rollup-plugin-copy';
+import dts from 'rollup-plugin-dts';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-    input: './src/index.ts',
-    external: ['lodash'],
-    output: {
-        file: 'dist/index.js',
-        format: 'umd',
-        name: 'jx3-luadata',
-        sourcemap: !production,
-        globals: {
-            lodash: 'lodash',
+export default [
+    {
+        input: './src/index.ts',
+        external: ['lodash'],
+        output: {
+            file: 'dist/index.js',
+            format: 'umd',
+            name: 'jx3-skill-parser',
+            sourcemap: true,
+            globals: {
+                lodash: 'lodash',
+            },
         },
+        plugins: [
+            typescript({
+                tsconfig: './tsconfig.json',
+                sourceMap: true,
+                declaration: false,
+            }),
+            json(),
+            copy({
+                targets: [{ src: 'src/lua', dest: 'dist' }],
+            }),
+        ],
     },
-    plugins: [
-        typescript({
-            sourceMap: !production,
-            outputToFilesystem: true,
-        }),
-        json(),
-        copy({
-            targets: [{ src: 'src/lua', dest: 'dist' }],
-        }),
-    ],
-};
+    {
+        input: './src/index.ts',
+        output: { file: 'dist/index.d.ts', format: 'es' },
+        plugins: [dts()],
+    },
+];
