11edddc5d57b5649454169a33c4424ccfb871bd5
X3ZvaWQ <x3zvawq@gmail.com>
2024-02-18 02:39:36 +0800

    fix: move_state mock

--- test/debug.mjs
+++ test/debug.mjs
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 
 const parser = await SkillParser.create();
 const content = await fs.readFile(
-    '/d/games/SeasunGame_unpack/std/scripts/skill/经脉/基础系/基础系_子技能_传送增强.lua',
+    '/d/games/SeasunGame_unpack/std/scripts/skill/明教/明教_暗尘弥散.lua',
 );
 
 console.log(await parser.parse(content));
