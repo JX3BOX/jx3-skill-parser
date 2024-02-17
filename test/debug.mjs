50c1b7e9d50d4e83c9d5d3bc357b1681578c7acf
X3ZvaWQ <x3zvawq@gmail.com>
2024-02-18 01:27:32 +0800

    fix: update game version

--- test/debug.mjs
+++ test/debug.mjs
@@ -2,6 +2,6 @@ import { SkillParser } from '../dist/index.js';
 import fs from 'fs/promises';
 
 const parser = await SkillParser.create();
-const content = await fs.readFile('./test/jianpo.lua');
+const content = await fs.readFile('/d/games/SeasunGame_unpack/std/scripts/skill/万花/万花_点穴截脉_商阳指.lua');
 
 console.log(await parser.parse(content));
